7492b78862ff7e1212b1edadeb583d7007cca0ce
Furkan Tunalı <furkan@nod.st>
2015-09-24 21:19:59 +0300

    fix line endings

--- nod/node.bash
+++ nod/node.bash
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 #export PATH="/usr/local/share/npm/bin:$PATH"
-alias npm-exec='PATH=$(npm bin):$PATH'
+alias npm-exec='PATH="$(npm bin):$PATH"'
 
 function nod-nodeUpdate() {
-	npm cache clean &&
-	npm install -g npm@latest &&
-	npm update -g
+	npm cache clean && npm install -g npm@latest && npm update -g
 }
